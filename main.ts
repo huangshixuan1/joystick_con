db616885365644d212bffe217466717f88ab0364
huangshixuan1 <98266496+huangshixuan1@users.noreply.github.com>
2023-02-25 13:34:06 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,8 +10,7 @@ KSB045.onBtnChanged(KSB045.btnName.C, KSB045.pushType.down, function () {
     basic.pause(50)
     basic.clearScreen()
 })
-// 9 向右轉
-KSB045.onBtnChanged(KSB045.btnName.D, KSB045.pushType.down, function () {
+function BUTd () {
     radio.sendNumber(9)
     basic.showLeds(`
         . . # . .
@@ -22,9 +21,8 @@ KSB045.onBtnChanged(KSB045.btnName.D, KSB045.pushType.down, function () {
         `)
     basic.pause(50)
     basic.clearScreen()
-})
-// 7 向左轉
-KSB045.onBtnChanged(KSB045.btnName.F, KSB045.pushType.down, function () {
+}
+function BUTf () {
     radio.sendNumber(7)
     basic.showLeds(`
         . . # . .
@@ -35,7 +33,7 @@ KSB045.onBtnChanged(KSB045.btnName.F, KSB045.pushType.down, function () {
         `)
     basic.pause(50)
     basic.clearScreen()
-})
+}
 KSB045.onBtnChanged(KSB045.btnName.E, KSB045.pushType.down, function () {
     radio.sendNumber(1)
     basic.showLeds(`
@@ -98,6 +96,10 @@ basic.forever(function () {
             `)
         basic.pause(50)
         basic.clearScreen()
+    } else if (KSB045.getBtnValue(KSB045.btnName.F)) {
+        BUTf()
+    } else if (KSB045.getBtnValue(KSB045.btnName.D)) {
+        BUTd()
     } else {
         radio.sendNumber(0)
     }
